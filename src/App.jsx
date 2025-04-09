caf5994aee30101d23a9a4181821661d54ab9fba
sharmilaraman <sharmila05122002@gmail.com>
2025-04-09 15:23:47 +0530

    changes is app,Home,Login,Xman files

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,18 @@ import Login from './components/Login';
 import Home from './components/Home';
 import TomcruiseObliviation from './components/TomcruiseObliviation';
 import Xmen from './components/Xmen';
+import Xman from './components/Xman';
+import Xman1 from './components/Xman1';
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/home" element={<Home />} />
-        <Route path='/TomcruiseObliviation' element={<TomcruiseObliviation />} />
-        <Route path='/Xmen' element={<Xmen />} />
+        <Route path='/movies/TomcruiseObliviation' element={<TomcruiseObliviation />} />
+        <Route path='/movies/Xmen' element={<Xmen />} />
+        <Route path='/movies/Xman' element={<Xman />} />
+        <Route path='/movies/Xman1' element={<Xman1 />} />
       </Routes>
    </>
   )
