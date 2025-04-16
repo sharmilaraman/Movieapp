8e193a7928ea247975ac75a3515d40c53dc15f59
sharmilaraman <sharmila05122002@gmail.com>
2025-04-16 18:42:25 +0530

    Initial Commit

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Navbar.css'
-import { useNavigate } from 'react-router-dom';
+import { useNavigate,Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 const Navbar = () => {
@@ -19,9 +19,9 @@ const Navbar = () => {
           <FontAwesomeIcon icon={faSearch} className="search-icon" />
           <input type="text" placeholder="Search" className="search-bar" />
         </div>
-        // <div className="user-link-container">
-        // <Link to="/user" className="user-link">User Data</Link>
-        // </div>
+         <div className="user-link-container">
+         <Link to="/user" className="user-link">User Data</Link>
+         </div>
         <div>
           <button className="logout-btn" onClick={handleLogout}>Log Out</button>
           </div>
